8ef0d79853edf2f4b055dac62b2e0e8b876426a8
RocioB <rociobarlo@gmail.com>
2014-12-23 17:43:48 +0100

    Añdadidos archivos basicos juego

---
+++ Assets/scripts/disparo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disparo : MonoBehaviour {
+
+
+
+	public GameObject bala;
+	public Transform puntoDeDisparo;
+
+
+	public void fire () {
+		if (bala != null) {
+			var clone = Instantiate(bala, puntoDeDisparo.position,
+			                        Quaternion.identity) as GameObject;
+			clone.transform.localScale = transform.localScale;
+				} else {
+			Debug.Log ("Sin balas") ;
+	}
+}
+}
