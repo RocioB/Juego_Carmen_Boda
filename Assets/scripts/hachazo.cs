ef8c04eb1539e1c879467ef0a36119c17964a0ad
RocioB <rociobarlo@gmail.com>
2015-02-14 01:40:27 +0100

    Nivel 1 jugable
    Añadido ataque [Boton derecho del raton]
    Añadido sistema de vida para los zombis
    Añadidos los limites del nivel

---
+++ Assets/scripts/hachazo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hachazo : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			animator.SetBool ("Hachazo", true);
+			
+		} else {
+			animator.SetBool ("Hachazo", false);
+		}
+	}
+}
