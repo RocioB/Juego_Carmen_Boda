9f56a5776ef4cd12048c652113049df87fd3964a
RocioB <rociobarlo@gmail.com>
2015-02-18 23:18:40 +0100

    añadidos sonidos y solucionado algunos bugs

---
+++ Assets/scripts/sonidoScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sonidoScript : MonoBehaviour {
+	public AudioClip cancion;
+	// Use this for initialization
+
+void OnTriggerEnter2D(Collider2D player){
+	
+	AudioSource.PlayClipAtPoint(cancion,transform.position);
+
+	}
+}
